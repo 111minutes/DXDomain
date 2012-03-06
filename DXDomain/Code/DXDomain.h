3ffd1fc19ff2b7eb8d81d37692ed848b7646ab9d
sergeyzenchenko <mail@sergey-zenchenko.com>
2012-03-06 12:53:49 +0200

    add project

--- DXDomain/Code/DXDomain.h
+++ DXDomain/Code/DXDomain.h
@@ -1,13 +1,8 @@
 //
-//  DXDomain.m
+//  DXDomain.h
 //  DXDomain
 //
 //  Created by Sergey Zenchenko on 3/6/12.
 //  Copyright (c) 2012 DIMALEX. All rights reserved.
 //
 
-#import "DXDomain.h"
-
-@implementation DXDomain
-
-@end
